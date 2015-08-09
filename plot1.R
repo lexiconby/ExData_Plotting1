3c205e8affd3286fbada92d53d98006e99aa32aa
Billy Yeh <lexiconby@users.noreply.github.com>
2015-08-09 15:46:37 -0700

    Update plot1.R
    added dimensions. can't seem to get the color to be translated to the plot() function from the hist function

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-#this script used to plot the plot1 graph#assuming that you downloaded the zip file and upzipped it into the working directory of
+#assuming that you downloaded the zip file and upzipped it into the working directory of
 #your R studio
 #read in data
 dataset <- read.delim("household_power_consumption.txt", header = TRUE, sep = ";")
@@ -7,9 +7,9 @@ dataset <- read.delim("household_power_consumption.txt", header = TRUE, sep = ";
 dataset_sub <- subset(dataset, dataset$Date == '2/1/2007' | dataset$Date == '2/2/2007')
 
 #draw histogram
-plot1out <- hist(as.numeric(dataset_sub$Global_active_power))
+plot1out <- hist(as.numeric(dataset_sub$Global_active_power), col="RED")
 
 #create png
-png(filename="plot1.png")
+png(filename="plot1.png", width=480, height=480, unitx="px", pointsize=12)
 plot(plot1out)
 dev.off()
