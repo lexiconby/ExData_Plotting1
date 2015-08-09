1cef9bb46e1cb88b4a29562eb243ff2473fa9c6f
Billy Yeh <lexiconby@users.noreply.github.com>
2015-08-09 15:52:39 -0700

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -6,10 +6,11 @@ dataset <- read.delim("household_power_consumption.txt", header = TRUE, sep = ";
 #create subset of only 2/1 and 2/2 rows
 dataset_sub <- subset(dataset, dataset$Date == '2/1/2007' | dataset$Date == '2/2/2007')
 
+#setup png filename
+png(filename="plot1.png", width=480, height=480, units="px", pointsize=12)
+
 #draw histogram
 plot1out <- hist(as.numeric(dataset_sub$Global_active_power), col="RED")
 
-#create png
-png(filename="plot1.png", width=480, height=480, unitx="px", pointsize=12)
-plot(plot1out)
+#close connection to file
 dev.off()
